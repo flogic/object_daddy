b4148654c32fca7415bc7499d88960bc73762779
Rick Bradley <rick@rickbradley.com>
2007-11-25 00:32:00 -0600

    create the exemplars directory when the plugin is installed

--- install.rb
+++ install.rb
@@ -1,3 +1,3 @@
-
+FileUtils.mkdir(File.dirname(__FILE__) + '/../../../test/exemplars') unless File.directory?(File.dirname(__FILE__) + '/../../../test/exemplars')
 
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
