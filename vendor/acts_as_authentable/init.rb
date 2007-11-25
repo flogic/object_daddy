b9510b6725df2828ff5357b93ffdddd690b60e45
Rick Bradley <rick@rickbradley.com>
2007-11-24 23:07:05 -0600

    removing this plugin I was using as an example

--- vendor/acts_as_authentable/init.rb
+++
@@ -1,7 +0,0 @@
-%w(acts_as_authentable authentable_entity authenticated_system).each do |file|
-  require file
-end
-
-ActionController::Base.send :include, AuthenticatedSystem
-ActionController::Base.send :filter_parameter_logging, :password
-ActiveRecord::Base.send :include, ActiveRecord::Acts::Authentable
