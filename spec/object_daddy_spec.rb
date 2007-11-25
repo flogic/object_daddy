28b4d618754febc8a5eaf7303095ba2f422403a1
Rick Bradley <rick@rickbradley.com>
2007-11-25 02:30:53 -0600

    update spec for readability

--- spec/object_daddy_spec.rb
+++ spec/object_daddy_spec.rb
@@ -18,125 +18,6 @@ describe ObjectDaddy, "when included into a class" do
 end
 
 
-# a dummy class, useful for testing the actual loading of exemplar files
-class Widget < OpenStruct
-end
-
-describe ObjectDaddy, "when generating a class instance" do
-  before(:each) do
-    @class = Class.new(OpenStruct)
-    @class.send(:include, ObjectDaddy)
-    @file_path = File.join(File.dirname(__FILE__), 'tmp')
-    @file_name = File.join(@file_path, 'widget_exemplar.rb')
-    @class.stubs(:exemplar_path).returns(@file_path)
-    @class.stubs(:name).returns('Widget')
-  end
-  
-  it "should register exemplars for the target class on the first attempt" do
-    @class.expects(:gather_exemplars)
-    @class.generate
-  end
-  
-  it "should not register exemplars for the target class after the first attempt" do
-    @class.generate
-    @class.expects(:gather_exemplars).never
-    @class.generate
-  end
-
-  it "should look for exemplars for the target class in the standard exemplar path" do
-    @class.expects(:exemplar_path).returns(@file_path)
-    @class.generate
-  end
-  
-  it "should look for an exemplar for the target class, based on the class's name" do
-    @class.expects(:name).returns('Widget')
-    @class.generate
-  end
-  
-  it "should register any generators found in the exemplar for the target class" do 
-    # we are using the concrete Widget class here because otherwise it's difficult to have our exemplar file work in our class
-    begin
-      File.open(@file_name, 'w') {|f| f.puts "class Widget\ngenerator_for :foo\nend\n"}
-        Widget.send :include, ObjectDaddy
-        Widget.stubs(:exemplar_path).returns(@file_path)
-        Widget.expects(:generator_for)
-        Widget.generate
-    ensure
-      # clean up test data file
-      File.unlink(@file_name) if File.exists?(@file_name)
-    end
-  end
-  
-  it "should register no generators if no exemplar for the target class is available" do
-    @class.expects(:generator_for).never
-    @class.generate
-  end
-  
-  it "should allow attributes to be overridden" do
-    @class.generate(:foo => 'xyzzy').foo.should == 'xyzzy'
-  end
-  
-  it "should not use a block generator for an attribute that has been overridden" do
-    @class.generator_for :foo do |prev| "foo"; end
-    @class.generate(:foo => 'xyzzy').foo.should == 'xyzzy'    
-  end
-  
-  it "should use any generators registered with blocks" do
-    @class.generator_for :foo do |prev| "foo"; end
-    @class.generate.foo.should == 'foo'
-  end
-  
-  it "should use any generators registered with generator method names" do
-    @class.stubs(:generator_method).returns('bar')
-    @class.generator_for :foo, :method => :generator_method
-    @class.generate.foo.should == 'bar'
-  end
-  
-  it "should not use a method generator for an attribute that has been overridden" do
-    @class.stubs(:generator_method).returns('bar')
-    @class.generator_for :foo, :method => :generator_method
-    @class.generate(:foo => 'xyzzy').foo.should == 'xyzzy'    
-  end
-  
-  it "should use any generators registered with generator classes" do
-    @generator_class = Class.new do
-      def self.next() 'baz' end
-    end
-    @class.generator_for :foo, :class => @generator_class
-    @class.generate.foo.should == 'baz'
-  end
-
-  it "should not use a class generator for an attribute that has been overridden" do
-    @generator_class = Class.new do
-      def self.next() 'baz' end
-    end
-    @class.generator_for :foo, :class => @generator_class
-    @class.generate(:foo => 'xyzzy').foo.should == 'xyzzy'    
-  end
-  
-  it "should return the initial value first if one was registered for a block generator" do
-    @class.generator_for :foo, :start => 'frobnitz' do |prev| "foo"; end
-    @class.generate.foo.should == 'frobnitz'
-  end
-  
-  it "should return the block applied to the initial value on the second call if an initial value was registered for a block generator" do
-    @class.generator_for :foo, :start => 'frobnitz' do |prev| prev.succ; end
-    @class.generate
-    @class.generate.foo.should == 'frobniua'
-  end
-  
-  it "should return the block applied to the previous value when repeatedly calling a block generator" do
-    @class.generator_for :foo do |prev| prev ? prev.succ : 'test'; end
-    @class.generate
-    @class.generate.foo.should == 'tesu'
-  end
-  
-  it "should call the normal target class constructor" do
-    @class.expects(:new)
-    @class.generate
-  end
-end
-
 describe ObjectDaddy, "when registering a generator method" do
   before(:each) do
     @class = Class.new(OpenStruct)
@@ -201,6 +82,125 @@ describe ObjectDaddy, "when registering a generator method" do
   end
 end
 
+# a dummy class, useful for testing the actual loading of exemplar files
+class Widget < OpenStruct
+end
+
+describe ObjectDaddy, "when generating a class instance" do
+  before(:each) do
+    @class = Class.new(OpenStruct)
+    @class.send(:include, ObjectDaddy)
+    @file_path = File.join(File.dirname(__FILE__), 'tmp')
+    @file_name = File.join(@file_path, 'widget_exemplar.rb')
+    @class.stubs(:exemplar_path).returns(@file_path)
+    @class.stubs(:name).returns('Widget')
+  end
+  
+  it "should register exemplars for the target class on the first attempt" do
+    @class.expects(:gather_exemplars)
+    @class.generate
+  end
+  
+  it "should not register exemplars for the target class after the first attempt" do
+    @class.generate
+    @class.expects(:gather_exemplars).never
+    @class.generate
+  end
+
+  it "should look for exemplars for the target class in the standard exemplar path" do
+    @class.expects(:exemplar_path).returns(@file_path)
+    @class.generate
+  end
+  
+  it "should look for an exemplar for the target class, based on the class's name" do
+    @class.expects(:name).returns('Widget')
+    @class.generate
+  end
+  
+  it "should register any generators found in the exemplar for the target class" do 
+    # we are using the concrete Widget class here because otherwise it's difficult to have our exemplar file work in our class
+    begin
+      File.open(@file_name, 'w') {|f| f.puts "class Widget\ngenerator_for :foo\nend\n"}
+        Widget.send :include, ObjectDaddy
+        Widget.stubs(:exemplar_path).returns(@file_path)
+        Widget.expects(:generator_for)
+        Widget.generate
+    ensure
+      # clean up test data file
+      File.unlink(@file_name) if File.exists?(@file_name)
+    end
+  end
+  
+  it "should register no generators if no exemplar for the target class is available" do
+    @class.expects(:generator_for).never
+    @class.generate
+  end
+  
+  it "should allow attributes to be overridden" do
+    @class.generate(:foo => 'xyzzy').foo.should == 'xyzzy'
+  end
+  
+  it "should use any generators registered with blocks" do
+    @class.generator_for :foo do |prev| "foo"; end
+    @class.generate.foo.should == 'foo'
+  end
+  
+  it "should not use a block generator for an attribute that has been overridden" do
+    @class.generator_for :foo do |prev| "foo"; end
+    @class.generate(:foo => 'xyzzy').foo.should == 'xyzzy'    
+  end
+  
+  it "should use any generators registered with generator method names" do
+    @class.stubs(:generator_method).returns('bar')
+    @class.generator_for :foo, :method => :generator_method
+    @class.generate.foo.should == 'bar'
+  end
+  
+  it "should not use a method generator for an attribute that has been overridden" do
+    @class.stubs(:generator_method).returns('bar')
+    @class.generator_for :foo, :method => :generator_method
+    @class.generate(:foo => 'xyzzy').foo.should == 'xyzzy'    
+  end
+  
+  it "should use any generators registered with generator classes" do
+    @generator_class = Class.new do
+      def self.next() 'baz' end
+    end
+    @class.generator_for :foo, :class => @generator_class
+    @class.generate.foo.should == 'baz'
+  end
+
+  it "should not use a class generator for an attribute that has been overridden" do
+    @generator_class = Class.new do
+      def self.next() 'baz' end
+    end
+    @class.generator_for :foo, :class => @generator_class
+    @class.generate(:foo => 'xyzzy').foo.should == 'xyzzy'    
+  end
+  
+  it "should return the initial value first if one was registered for a block generator" do
+    @class.generator_for :foo, :start => 'frobnitz' do |prev| "foo"; end
+    @class.generate.foo.should == 'frobnitz'
+  end
+  
+  it "should return the block applied to the initial value on the second call if an initial value was registered for a block generator" do
+    @class.generator_for :foo, :start => 'frobnitz' do |prev| prev.succ; end
+    @class.generate
+    @class.generate.foo.should == 'frobniua'
+  end
+  
+  it "should return the block applied to the previous value when repeatedly calling a block generator" do
+    @class.generator_for :foo do |prev| prev ? prev.succ : 'test'; end
+    @class.generate
+    @class.generate.foo.should == 'tesu'
+  end
+  
+  it "should call the normal target class constructor" do
+    @class.expects(:new)
+    @class.generate
+  end
+end
+
 # conditionally do Rails tests, if we were included as a plugin
 if File.exists?("#{File.dirname(__FILE__)}/../../../../config/environment.rb")
 
@@ -240,7 +240,7 @@ if File.exists?("#{File.dirname(__FILE__)}/../../../../config/environment.rb")
       Frobnitz.generate
     end
     
-    it "should not generate instance of belongs_to associations which are not required by a presence_of validator" do
+    it "should not generate instances of belongs_to associations which are not required by a presence_of validator" do
       Bar.expects(:generate).never
       Frobnitz.generate
     end
