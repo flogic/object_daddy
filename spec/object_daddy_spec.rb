b3f92fe2d3b9d3e3e0306be70ecfa5765cddc88e
Yossef Mendelssohn <ymendel@pobox.com>
2008-02-19 12:36:14 -0600

    Adding specs to ensure blocks that specifically take no args (arity of 0 instead of -1 in Ruby 1.8) work just as well as blocks that implicitly take no arguments.

--- spec/object_daddy_spec.rb
+++ spec/object_daddy_spec.rb
@@ -53,6 +53,10 @@ describe ObjectDaddy, "when registering a generator method" do
     lambda { @class.generator_for :foo, :first => 'baz' do end }.should_not raise_error(ArgumentError)
   end
   
+  it "should not fail if a generator block specifically doesn't handle a previous value" do
+    lambda { @class.generator_for :foo, :first => 'baz' do || end }.should_not raise_error(ArgumentError)
+  end
+  
   it "should fail if a generator block expects more than one argument" do
     lambda { @class.generator_for :foo, :first => 'baz' do |x, y| end }.should raise_error(ArgumentError)
   end
@@ -250,6 +254,12 @@ describe ObjectDaddy, "when generating a class instance" do
   it 'should use the return value for a block generator that takes no argument' do
     x = 5
     @class.generator_for(:foo) { x }
+    @class.generate.foo.should == x
+  end
+  
+  it 'should use the return value for a block generator that specifically takes no argument' do
+    x = 5
+    @class.generator_for(:foo) { ||  x }
     @class.generate.foo.should == x
   end
   
